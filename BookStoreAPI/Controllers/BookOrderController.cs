a8bdbc936658a1c74e897739b620ed6e00b0429c
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-23 20:48:09 +0300

    add web api controller

---
+++ BookStoreAPI/Controllers/BookOrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers
+{
+    [Route("api/book_order")]
+    [ApiController]
+    public class BookOrderController : ControllerBase
+    {
+    }
+}
