ec0364c8ba49309bf8752301df948edcc2421103
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 21:50:12 +0300

    Configure mapper

--- BookStoreAPI/Program.cs
+++ BookStoreAPI/Program.cs
@@ -1,5 +1,8 @@
 
+using AutoMapper;
 using BookStoreAPI.Data;
+using BookStoreAPI.Data.Repository;
+using BookStoreAPI.Data.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookStoreAPI
@@ -16,8 +19,23 @@ namespace BookStoreAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            
+            //Configure db connection
             builder.Services.AddDbContext<BookStoreContext>(opts =>
                 opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            //Configure AutoMapper
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AppMappingProfile>();
+            });
+
+            var mapper = mapperConfig.CreateMapper();
+
+            //Configure DI
+            builder.Services.AddScoped<IBookRepository, BookRepository>();
+            builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+            builder.Services.AddSingleton(mapper);
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
