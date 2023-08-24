076806f000929ab73a4dfb86d2188b1576355a1d
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 23:10:19 +0300

    Add order validation middleware

---
+++ BookStoreAPI/Middlewares/OrderValidationMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using BookStoreAPI.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace BookStoreAPI.Middlewares
+{
+    public class OrderValidationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public OrderValidationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            string requestBody = await new StreamReader(httpContext.Request.Body).ReadToEndAsync();
+
+            OrderModel order = JsonConvert.DeserializeObject<OrderModel>(requestBody);
+
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(order, new ValidationContext(order), validationResults, true);
+
+            if (!isValid)
+            {
+                var errors = validationResults.Select(vr => vr.ErrorMessage);
+                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                httpContext.Response.ContentType = "application/json";
+                await httpContext.Response.WriteAsync(JsonConvert.SerializeObject(new {Errors = errors}));
+                return;
+            }
+            await _next(httpContext);
+        }
+    }
+
+    public static class OrderValidationMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseOrderValidationMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<OrderValidationMiddleware>();
+        }
+    }
+}
