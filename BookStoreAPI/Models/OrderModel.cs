6f9b0cbd1cc31b7f48dd8906ee29425730bccbcc
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 23:48:07 +0300

    Add data anotations for order model

--- BookStoreAPI/Models/OrderModel.cs
+++ BookStoreAPI/Models/OrderModel.cs
@@ -1,11 +1,19 @@
 ﻿using BookStoreAPI.Data.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStoreAPI.Models
 {
     public class OrderModel
     {
+        [Required]
         public Guid Id { get; private set; } = Guid.NewGuid();
+
+        [Required(ErrorMessage = "Order date is required")]
+        [Display(Name = "Order Date")]
         public DateTime OrderDate { get; set; }
+
+        [Required(ErrorMessage = "At least one book is required")]
+        [MinLength(1, ErrorMessage = "At least one book is required")]
         public List<BookModel> Books { get; set; }
     }
 }
