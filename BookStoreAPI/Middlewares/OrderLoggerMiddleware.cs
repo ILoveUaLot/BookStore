40bc7cef13183a29e86a79bb075496c979808781
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-25 01:52:53 +0300

    Add commentaries

--- BookStoreAPI/Middlewares/OrderLoggerMiddleware.cs
+++ BookStoreAPI/Middlewares/OrderLoggerMiddleware.cs
@@ -22,14 +22,18 @@ namespace BookStoreAPI.Middlewares
             _logger = logger;
         }
 
+        // The InvokeAsync method is responsible for handling the incoming HTTP request
         public async Task InvokeAsync(HttpContext httpContext)
         {
+            // Check if the incoming request is a POST request to the "/api/order" endpoint
             if (httpContext.Request.Path.StartsWithSegments("/api/order", StringComparison.OrdinalIgnoreCase) &&
                 httpContext.Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase))
             {
+                // Read the request body, expecting JSON content
                 var requestBody = await new StreamReader(httpContext.Request.Body).ReadToEndAsync();
                 var order = JsonConvert.DeserializeObject<OrderModel>(requestBody);
 
+                // Log information about the order and the ordered books
                 _logger.LogInformation($"Made order: {order.Id}; " +
                     $"Order accepted for processing {order.OrderDate}");
                 foreach (var item in order.Books)
@@ -37,7 +41,6 @@ namespace BookStoreAPI.Middlewares
                     _logger.LogInformation($"{item.Title} was ordered; it is id {item.Id}");
                 }
             }
-            await _next.Invoke(httpContext);
         }
     }
 
