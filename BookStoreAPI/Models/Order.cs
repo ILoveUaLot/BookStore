1088941155935966b7c406399339af80fc877b76
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 20:47:44 +0300

    Add order model

---
+++ BookStoreAPI/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreAPI.Models
+{
+    public class Order
+    {
+        public Guid id { get; init; }
+        public DateTime OrderDate { get; set; }
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+}
