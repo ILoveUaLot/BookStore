5a872b94ecd914fdf5935e485010a9ac37317589
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 23:49:50 +0300

    Add data annotations for book model

--- BookStoreAPI/Models/BookModel.cs
+++ BookStoreAPI/Models/BookModel.cs
@@ -1,11 +1,23 @@
-﻿namespace BookStoreAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreAPI.Models
 {
     public class BookModel
     {
+        [Required]
         public Guid Id { get; private set; } = Guid.NewGuid();
+
+        [Required(ErrorMessage = "Author is required")]
         public string Author { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
+
+        [MaxLength(500, ErrorMessage = "Description can't exceed 500 characters")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Release date is required")]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
     }
