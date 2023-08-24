3c2b822f5e671d6b72458291d2aa6a55e4e1ba2b
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 21:33:28 +0300

    Add bookrepository

---
+++ BookStoreAPI/Data/Repository/BookRepository.cs
@@ -0,0 +1,43 @@
+﻿using BookStoreAPI.Data.Repository.Interfaces;
+using BookStoreAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreAPI.Data.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        public readonly BookStoreContext db;
+        public BookRepository(BookStoreContext dbContext)
+        {
+            db = dbContext;
+        }
+        public async Task CreateAsync(Book entity)
+        {
+            await db.Books.AddAsync(entity);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Book entity)
+        {
+            db.Books.Remove(entity);
+            await db.SaveChangesAsync();
+        }
+
+        public IQueryable<Book> GetAll()
+        {
+            return db.Books;
+        }
+
+        public async Task<Book> GetByIdAsync(Guid id)
+        {
+            return await db.Books.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Book entity)
+        {
+            db.Books.Update(entity);
+            await db.SaveChangesAsync();
+        }
+
+    }
+}
