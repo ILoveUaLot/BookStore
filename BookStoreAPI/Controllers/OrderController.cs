896927a78e1279fa50f924846d46648d6d18d068
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 22:35:17 +0300

    Add get by filter

--- BookStoreAPI/Controllers/OrderController.cs
+++ BookStoreAPI/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using BookStoreAPI.Data.Entities;
 using BookStoreAPI.Data.Repository.Interfaces;
 using BookStoreAPI.Models;
 using Microsoft.AspNetCore.Http;
@@ -19,9 +20,12 @@ namespace BookStoreAPI.Controllers
         }
 
         [HttpGet("{id}&{orderDate}")]
-        public async Task<List<OrderModel>> GetOrderByFilter(Guid id, DateTime orderDate)
+        public async Task<List<OrderModel>> GetOrderByFilter(Guid id, DateTime? orderDate)
         {
-
+            List<Order> orderEntities = await _orderRepo.GetOrdersByFilter(id, orderDate);
+            if (orderEntities == null) NotFound();
+            List<OrderModel> orderList = _mapper.Map<List<OrderModel>>(orderEntities);
+            return orderList;
         }
     }
 }
