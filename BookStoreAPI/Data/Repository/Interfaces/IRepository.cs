5c026c6b0e0201abbf17901cf643d109b0a7b4ec
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 21:31:17 +0300

    change updateasync

--- BookStoreAPI/Data/Repository/Interfaces/IRepository.cs
+++ BookStoreAPI/Data/Repository/Interfaces/IRepository.cs
@@ -3,7 +3,7 @@
     public interface IRepository<T> where T : class
     {
         Task CreateAsync(T entity);
-        Task UpdateAsync();
+        Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
         Task<T> GetByIdAsync(Guid id);
         IQueryable<T> GetAll();
