d5c129f6d13f1094d9fc6a9ac53acf3a2c2fa721
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 20:51:47 +0300

    Add IRepository

---
+++ BookStoreAPI/Data/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStoreAPI.Data.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<T> GetByIdAsync(Guid id);
+        Task<IQueryable<T>> GetAllAsync();
+    }
+}
