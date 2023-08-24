d3a482d3408f9c2b86195f11062445e9e5d7267b
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 21:36:47 +0300

    Add order repository

---
+++ BookStoreAPI/Data/Repository/OrderRepository.cs
@@ -0,0 +1,42 @@
+﻿using BookStoreAPI.Data.Repository.Interfaces;
+using BookStoreAPI.Models;
+
+namespace BookStoreAPI.Data.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public readonly BookStoreContext db;
+        public OrderRepository(BookStoreContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task CreateAsync(Order entity)
+        {
+            await db.Orders.AddAsync(entity);    
+            await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Order entity)
+        {
+            db.Orders.Remove(entity);
+            await db.SaveChangesAsync();
+        }
+
+        public IQueryable<Order> GetAll()
+        {
+            return db.Orders;
+        }
+
+        public async Task<Order> GetByIdAsync(Guid id)
+        {
+           return await db.Orders.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Order entity)
+        {
+            db.Orders.Update(entity);
+            await db.SaveChangesAsync();
+        }
+    }
+}
