343549275bb9e22ba64422dd62aa483b462631d9
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 21:01:17 +0300

    Add bookstore context

---
+++ BookStoreAPI/Data/BookStoreContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStoreAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreAPI.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions opts) : base(opts)
+        {
+
+        }
+
+        DbSet<Book> Books { get; set; }
+        DbSet<Order> Orders { get; set; }
+    }
+}
