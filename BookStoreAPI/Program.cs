eae31e85b52bb76445ede993765a58e79bd23b8d
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 21:05:41 +0300

    Add connection to db

--- BookStoreAPI/Program.cs
+++ BookStoreAPI/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace BookStoreAPI
 {
     public class Program
@@ -13,7 +15,8 @@ namespace BookStoreAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<BookStoreContext>(opts =>
+                opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
