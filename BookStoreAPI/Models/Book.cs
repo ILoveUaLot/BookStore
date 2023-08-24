a3f69f006182dba2bd50b391afb8b3a0707e0ab8
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 20:46:38 +0300

    Add book model

---
+++ BookStoreAPI/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStoreAPI.Models
+{
+    public class Book
+    {
+        public Guid id { get; init; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
