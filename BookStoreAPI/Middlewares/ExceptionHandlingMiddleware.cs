579adf6f1ea861ad721525198db3f4c906a4ea27
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-24 22:54:41 +0300

    Add exception middleware

---
+++ BookStoreAPI/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿namespace BookStoreAPI.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        ILogger _logger;
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "An unhadled exception occured");
+
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                httpContext.Response.ContentType = "application/json";
+                await httpContext.Response.WriteAsync("An error occurred while processing the request.");
+            }
+        }
+    }
+
+    public static class ExceptionHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
